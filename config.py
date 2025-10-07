5c749daa478959b69c538ff26e69b92c995d39d5
bismillah626 <khanbismillah2004@gmail.com>
2025-10-07 19:35:19 +0530

    config file setup dn

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#API keys 
+GEMINI_API_KEYS = os.getenv("GEMINI_API_KEYS")
+HUGGINGFACE_API_KEYS = os.getenv("HUGGINGFACE_API_KEYS")  
+  
+#spotify client setup
+SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")  
