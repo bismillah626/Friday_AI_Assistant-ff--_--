ce4fdb4c295662b3fb884a588010ce55e95931bb
bismillah626 <khanbismillah2004@gmail.com>
2025-10-10 12:10:51 +0530

    updated code

--- core/llm_engine.py
+++ core/llm_engine.py
@@ -1,11 +1,19 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from config import GOOGLE_API_KEY
 
-def get_gemini_llm():
-    """Initialize and return the Gemini LLM."""
+def get_pro_llm():
+    """Initialize and return the Gemini-2.5-pro-LLM."""
     return ChatGoogleGenerativeAI(
-        model = "gemini-pro",
+        model = "gemini-2.5-pro-latest",
         google_api_key = GOOGLE_API_KEY,
         temperature = 0.75,
         convert_system_message_to_human=True 
         )
+def get_flash_llm():
+    """Initialize and return the Gemini-2.5-flash-LLM."""
+    return ChatGoogleGenerativeAI(
+        model = "gemini-2.5-flash-latest",
+        google_api_key = GOOGLE_API_KEY,
+        temperature = 0.75,
+        convert_system_message_to_human=True     
+    )
